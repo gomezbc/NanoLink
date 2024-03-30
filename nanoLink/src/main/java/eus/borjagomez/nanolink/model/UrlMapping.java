c74094207c91d0948667f9a6e0dffe43438315c0
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-03-30 22:29:12 +0100

    (fix) asserted cassandra table schema and model schema

--- nanoLink/src/main/java/eus/borjagomez/nanolink/model/UrlMapping.java
+++ nanoLink/src/main/java/eus/borjagomez/nanolink/model/UrlMapping.java
@@ -3,27 +3,38 @@ package eus.borjagomez.nanolink.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.sql.Timestamp;
 
-@Table
+@Table("url_mappings")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UrlMapping {
 
     @PrimaryKey
+    @Column("mapping_id")
     private String mappingId;
+    @Column("long_url")
     private String longUrl;
+    @Column("short_url")
     private String shortUrl;
+    @Column("created_date")
     private Timestamp createdDate;
+    @Column("updated_date")
     private Timestamp updatedDate;
+    @Column("expiry_date")
     private Timestamp expiryDate;
+    @Column("last_hit_date")
     private Timestamp lastHitDate;
+    @Column("hit_count")
     private int hitCount;
+    @Column("created_by")
     private String createdBy;
+    @Column("updated_by")
     private String updatedBy;
 
 }
