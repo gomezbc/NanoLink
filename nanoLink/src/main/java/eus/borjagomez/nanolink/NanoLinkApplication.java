0c1f0f22a1ba1df4648de1450a6c443c1652491d
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-03-31 21:13:58 +0200

    (fix) connection for kubernetes client discover

--- nanoLink/src/main/java/eus/borjagomez/nanolink/NanoLinkApplication.java
+++ nanoLink/src/main/java/eus/borjagomez/nanolink/NanoLinkApplication.java
@@ -2,8 +2,10 @@ package eus.borjagomez.nanolink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NanoLinkApplication {
 
     public static void main(String[] args) {
