5585b80594df5de3a109894a0d283091790368d9
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-08-11 13:56:24 +0200

    refactor: remove unnecessary code

--- src/main/java/eus/borjagomez/nanolink/mapper/UrlMappingMapper.java
+++ src/main/java/eus/borjagomez/nanolink/mapper/UrlMappingMapper.java
@@ -3,9 +3,7 @@ package eus.borjagomez.nanolink.mapper;
 import eus.borjagomez.nanolink.dto.CreateUrlMappingDto;
 import eus.borjagomez.nanolink.dto.UpdateUrlMappingDto;
 import eus.borjagomez.nanolink.model.UrlMapping;
-import eus.borjagomez.nanolink.utils.UrlParser;
 
-import java.net.MalformedURLException;
 import java.sql.Timestamp;
 
 public class UrlMappingMapper {
@@ -13,12 +11,6 @@ public class UrlMappingMapper {
     public static UrlMapping mapToUrlMapping(CreateUrlMappingDto createUrlMappingDto, UrlMapping urlMapping) {
         urlMapping.setMappingId(createUrlMappingDto.getMappingId());
         urlMapping.setLongUrl(createUrlMappingDto.getLongUrl());
-        String parsedUrl;
-        try {
-            parsedUrl = UrlParser.removeUrlPath(createUrlMappingDto.getLongUrl());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
         urlMapping.setCreatedDate(new Timestamp(System.currentTimeMillis()));
         Timestamp expiryDate = createUrlMappingDto.getExpiryDate();
         if (expiryDate != null) {
--- src/test/java/eus/borjagomez/nanolink/NanoLinkApplicationTests.java
+++ src/test/java/eus/borjagomez/nanolink/NanoLinkApplicationTests.java
@@ -1,13 +1,8 @@
 package eus.borjagomez.nanolink;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NanoLinkApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
